a583187c28844c7153b45e4920ff1505292b4e79
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-13 14:05:12 -0400

    added style to roll dice btn

--- src/components/Die.js
+++ src/components/Die.js
@@ -4,7 +4,7 @@
 export default function Die(props) {
     return (
         <div className="dice">
-            <h2>{props.value}</h2>
+            <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
