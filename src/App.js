d589957fb8108192e70f735fe0a5300882df1ad8
Daniel Braaksma <danebraaksma@hotmail.com>
2022-04-13 09:47:01 -0400

    added dice component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react"
+import Dice from "./components/Dice"
 
 export default function App () {
     return (
         <main>
-            
+            <div className="dice-container">
+                <Dice />
+                <Dice />
+                <Dice />
+                <Dice />
+                <Dice />
+                <Dice />
+                <Dice />
+                <Dice />
+                <Dice />
+                <Dice />
+            </div>
         </main>
     )
 }
---
+++ src/components/Dice.js
@@ -0,0 +1,10 @@
+// import React from "react"
+
+
+export default function Dice() {
+    return (
+        <div className="dice">
+            <p>1</p>
+        </div>
+    )
+}
